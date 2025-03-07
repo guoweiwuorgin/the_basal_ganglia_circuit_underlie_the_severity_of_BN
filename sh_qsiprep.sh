8a1a59d7318c41aa516891f6889b128e83be174d
guoweiwuorgin <53160585+guoweiwuorgin@users.noreply.github.com>
2025-03-07 21:42:29 +0800

    Update sh_qsiprep.sh

--- sh_qsiprep.sh
+++ sh_qsiprep.sh
@@ -1,4 +1,4 @@
 for file in `cat subname.txt`
 do
- sbatch -p q_fat -c 10 -e ${file}_error.txt -o ${file}_out.txt ind_qsiprep.sh $file
+ sbatch -p q_Node_bj -c 10 -e ${file}_error.txt -o ${file}_out.txt ind_qsiprep.sh $file
 done
